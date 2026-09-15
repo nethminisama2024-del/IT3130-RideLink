b21f06581efa42f01458be5bc50204a42025443e
Samadhi Nethmini <nethminisama2024@gmail.com>
2026-09-15 12:28:18 +0530

    feat: add vehicle entity and vehicle type

---
+++ driver-service/src/main/java/com/ridelink/driverservice/entity/Vehicle.java
@@ -0,0 +1,90 @@
+package com.ridelink.driverservice.entity;
+
+import com.ridelink.driverservice.enums.VehicleType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long driverId;
+
+    @Column(nullable = false, unique = true)
+    private String registrationNumber;
+
+    @Column(nullable = false)
+    private String make;
+
+    @Column(nullable = false)
+    private String model;
+
+    private String colour;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleType vehicleType;
+
+    public Vehicle() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+}
---
+++ driver-service/src/main/java/com/ridelink/driverservice/enums/VehicleType.java
@@ -0,0 +1,8 @@
+package com.ridelink.driverservice.enums;
+
+public enum VehicleType {
+    CAR,
+    VAN,
+    THREE_WHEELER,
+    MOTORBIKE
+}
